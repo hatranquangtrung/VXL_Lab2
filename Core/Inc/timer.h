5dd3943de700394641a22f55486d8b51d7bfe651
Ha Tran Quang Trung <quangtrunght02@gmailcom>
2024-11-30 10:17:38 +0700

    Ex6

--- Core/Inc/timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef INC_TIMER_H_
-#define INC_TIMER_H_
-
-extern int timer1_flag;
-void setTimer1(int duration);
-void timer_run1();
-
-#endif
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,7 +19,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "timer.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
